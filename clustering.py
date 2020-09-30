84b0807f781cbf6a98689e4ce4ee84ef9a68f30d
mrcrysmile <275076698@qq.com>
2020-09-30 14:31:04 +0800

    1

--- clustering.py
+++ clustering.py
@@ -11,9 +11,34 @@ x, y = make_blobs(500, 2, 4, random_state=3)
 
 # ax1.scatter(x[:, 0], x[:, 1], s=8)
 
-color = ['red', 'green', 'yellow', 'blue']
-fig, ax1 = plt.subplots(1)
-for i in range(4):
-    ax1.scatter(x[y==i, 0], x[y==i, 1], c=color[i], s=8)
-plt.show()
+# color = ['red', 'green', 'yellow', 'blue']
+# fig, ax1 = plt.subplots(1)
+# for i in range(4):
+#     ax1.scatter(x[y==i, 0], x[y==i, 1], c=color[i], s=8)
+# plt.show()
+
+from sklearn.cluster import KMeans
+
+k = KMeans(n_clusters=3, random_state=3)
+l = k.fit(x)
+l.labels_
+l.cluster_centers_
+l.inertia_
+# help(l)
+
+pre = l.fit_predict(x)
+
+# color = ['red', 'green', 'yellow', 'blue']
+# fig, ax1 = plt.subplots(1)
+# for i in range(4):
+#     ax1.scatter(x[pre==i, 0], x[pre==i, 1], c=color[i], s=8)
+# plt.show()
+
+# 计算轮廓系数
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+silhouette_score(x, pre)
+silhouette_score(x, l.labels_)
+silhouette_samples(x, pre)
+
 # %%
